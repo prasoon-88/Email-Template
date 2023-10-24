8bfc45caf5e0cd696d71f8598e14c8adea0ddb5e
prasoon-88 <prasouna@gmail.com>
2023-10-24 22:59:20 +0530

    Added Hover effect on Card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const Card = ({card}) => {
+const Card = ({ card }) => {
     return (
         <div className="card">
             <div className="imageContainer">
                 {
-                    card.popularity > 10 ? <img src="/assets/crown.png" className="crown" />:'' 
+                    card.popularity > 10 ? <img src="/assets/crown.png" className="crown" /> : ''
                 }
+                <div className="btnContainer">
+                    <button className='useThis'>Use this</button>
+                </div>
                 <img src={card.img} alt="" />
             </div>
             <div>
